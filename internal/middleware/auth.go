737c03e2580e6b2a10a739d77abb320acd986d85
kkonst40 <161722238+kkonst40@users.noreply.github.com>
2026-01-12 02:44:45 +0300

    Auth middleware modified

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,64 +1,74 @@
 package middleware
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/google/uuid"
 	"github.com/kkonst40/ichat/internal/apperror"
 	"github.com/kkonst40/ichat/internal/config"
 	"github.com/kkonst40/ichat/internal/logger"
 )
 
-type CustomClaims struct {
-	ID       string `json:"id"`
-	UserName string `json:"userName"`
-	TokenID  string `json:"tokenId"`
-	jwt.StandardClaims
+type UserClaims struct {
+	ID       uuid.UUID `json:"id"`
+	UserName string    `json:"userName"`
+	TokenID  uuid.UUID `json:"tokenId"`
+	jwt.RegisteredClaims
 }
 
 func Auth(cfg *config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+		log := logger.FromContext(ctx)
+
 		tokenString, err := c.Cookie(cfg.JWT.CookieName)
 		if err != nil {
-			c.Error(&apperror.UnauthorizedError{Msg: "Cookie not found"})
+			c.Error(&apperror.UnauthorizedError{Msg: "Token not found"})
 			c.Abort()
 			return
 		}
 
-		token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(t *jwt.Token) (any, error) {
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, &apperror.UnauthorizedError{Msg: fmt.Sprintf("unexpected signing method: %v", t.Header["alg"])}
+		log.Info("", "token", tokenString)
+
+		claims, err := validateToken(tokenString, cfg)
+		if err != nil {
+			log.Error("Token validation error", "error", err.Error())
+			c.Error(&apperror.UnauthorizedError{Msg: "Invalid token"})
+			c.Abort()
+			return
+		}
+		c.Set("requesterID", claims.ID.String())
+
+		log.Info("", "userID", claims.ID)
+
+		c.Next()
+	}
+}
+
+func validateToken(tokenString string, cfg *config.Config) (*UserClaims, error) {
+	claims := &UserClaims{}
+
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+		func(token *jwt.Token) (any, error) {
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, jwt.ErrSignatureInvalid
 			}
 			return []byte(cfg.JWT.SecretKey), nil
-		})
+		},
+		jwt.WithIssuer(cfg.JWT.Issuer),
+		jwt.WithAudience(cfg.JWT.Audience),
+	)
 
-		if err != nil {
-			c.Error(&apperror.UnauthorizedError{Msg: "Invalid token"})
-			c.Abort()
-			return
-		}
-
-		if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
-			if claims.Issuer != cfg.JWT.Issuer &&
-				claims.Audience != cfg.JWT.Audience &&
-				claims.ExpiresAt > time.Now().Unix() {
-				c.Error(&apperror.UnauthorizedError{Msg: "Invalid token"})
-				c.Abort()
-				return
-			}
-
-			c.Set("requesterID", claims.ID)
-
-			ctx := c.Request.Context()
-			logger.FromContext(ctx).Info("", "userID", claims.ID)
-
-			c.Next()
-		} else {
-			c.Error(&apperror.UnauthorizedError{Msg: "Invalid token"})
-			c.Abort()
-			return
-		}
+	if err != nil {
+		return nil, err
 	}
+
+	if !token.Valid {
+		return nil, jwt.ErrTokenInvalidClaims
+	}
+
+	return claims, nil
 }
