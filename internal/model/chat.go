5b8f45f937f068846fd64976db9cdb0ff4735f9a
kkonst40 <161722238+kkonst40@users.noreply.github.com>
2026-02-28 03:47:04 +0300

    Chat model has last message time field

--- internal/model/chat.go
+++ internal/model/chat.go
@@ -1,8 +1,13 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Chat struct {
-	ID   uuid.UUID `json:"id"`
-	Name string    `json:"name"`
+	ID            uuid.UUID `json:"id"`
+	Name          string    `json:"name"`
+	LastMessageAt time.Time `json:"lastMessageAt"`
 }
