934b7e1731a96ff44cdcbb5ad09dcbd80433f8a6
kkonst40 <161722238+kkonst40@users.noreply.github.com>
2026-03-02 00:07:20 +0300

    GetChatUserIDs method added to repositories

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/google/uuid"
-	"github.com/kkonst40/ichat/internal/model"
+	"github.com/kkonst40/ichat/internal/domain/model"
 )
 
 type UserRepository interface {
 	GetChatUser(ctx context.Context, chatID, userID uuid.UUID) (*model.User, error)
 	GetChatUsers(ctx context.Context, chatID uuid.UUID) ([]model.User, error)
+	GetChatUserIDs(ctx context.Context, chatID uuid.UUID) ([]uuid.UUID, error)
 	AddChatUsers(ctx context.Context, chatID uuid.UUID, userIDs []uuid.UUID) error
 	DeleteChatUser(ctx context.Context, chatID uuid.UUID, userID uuid.UUID) error
 	UpdateUserRole(ctx context.Context, chatID, userID uuid.UUID, newRole model.Role) error
